02edbec49a0b87c1fe1ce84fd599487b86fadcaf
Cezar Halmagean <cezar@mixandgo.com>
2017-08-23 16:27:47 +0300

    Initial commit

---
+++ lib/mobilpay.ex
@@ -0,0 +1,18 @@
+defmodule Mobilpay do
+  @moduledoc """
+  Documentation for Mobilpay.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Mobilpay.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/mobilpay_test.exs
@@ -0,0 +1,8 @@
+defmodule MobilpayTest do
+  use ExUnit.Case
+  doctest Mobilpay
+
+  test "greets the world" do
+    assert Mobilpay.hello() == :world
+  end
+end
