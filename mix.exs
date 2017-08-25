6c3a3ac7874864574f66fd04863134412ede95ec
Cezar Halmagean <cezar@mixandgo.com>
2017-08-25 12:07:08 +0300

    Add deps and paths

--- mix.exs
+++ mix.exs
@@ -6,23 +6,30 @@ defmodule Mobilpay.Mixfile do
       app: :mobilpay,
       version: "0.1.0",
       elixir: "~> 1.5",
+      elixirc_paths: elixirc_paths(Mix.env),
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :timex]
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:xml_builder, "~> 0.1.1"},
+      {:sweet_xml, "~> 0.6"},
+      {:timex, "~> 3.1"},
+      {:ecto, "~> 2.2"}
     ]
   end
 end
