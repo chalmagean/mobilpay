a32401e5955c493bb326bffe1e397e4099ba5303
Cezar Halmagean <cezar@mixandgo.com>
2017-08-25 12:06:32 +0300

    Remove default function

--- lib/mobilpay.ex
+++ lib/mobilpay.ex
@@ -1,18 +1,2 @@
 defmodule Mobilpay do
-  @moduledoc """
-  Documentation for Mobilpay.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Mobilpay.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
