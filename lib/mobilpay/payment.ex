6ceaa8367ee06acf1934f25c1c1236a0f4c9ef5c
Cezar Halmagean <cezar@mixandgo.com>
2017-08-28 20:02:36 +0300

    Change Payment from an Ecto.Schema to an Elixir struct

--- lib/mobilpay/payment.ex
+++ lib/mobilpay/payment.ex
@@ -1,28 +1,25 @@
 defmodule Mobilpay.Payment do
-  use Ecto.Schema
-  import Ecto.Changeset
+  defstruct [
+    signature: "",
+    amount: "",
+    currency: "RON",
+    timezone: "Europe/Bucharest",
+    token: "",
+    email: "",
+    type: "person",
+    first_name: "",
+    last_name: "",
+    address: "",
+    mobile_phone: "",
+    confirm_url: "",
+    return_url: "",
+  ]
 
-  schema "payment" do
-    field :signature
-    field :amount, :integer
-    field :currency, :float, default: 0.0
-    field :timezone
-    field :token
-    field :email
-    field :type
-    field :first_name
-    field :last_name
-    field :address
-    field :mobile_phone
-    field :confirm_url
-    field :return_url
-  end
+  @type t :: %__MODULE__{}
 
-  def changeset(payment, params \\ %{}) do
-    payment
-    |> cast(params, [:signature, :amount, :currency, :timezone, :token, :email, :type, :first_name, :last_name, :address, :mobile_phone, :confirm_url, :return_url])
-    |> validate_required([:signature, :amount, :currency, :token, :email, :type, :confirm_url, :return_url])
-    |> validate_format(:email, ~r/@/)
-    |> validate_inclusion(:type, ["company", "person"])
-  end
+  @doc """
+  Returns the default Payment struct
+  """
+  @spec new() :: t
+  def new(), do: %__MODULE__{}
 end
--- test/mobilpay/payment_test.exs
+++ test/mobilpay/payment_test.exs
@@ -3,15 +3,21 @@ defmodule Mobilpay.PaymentTest do
 
   alias Mobilpay.Payment
 
-  test "changeset/2 validates email format" do
-    changeset = Payment.changeset(%Payment{}, %{email: "foo"})
-    refute changeset.valid?
-    assert Keyword.has_key?(changeset.errors, :email)
-  end
-
-  test "changeset/2 validates type set" do
-    changeset = Payment.changeset(%Payment{}, %{type: "foo"})
-    refute changeset.valid?
-    assert Keyword.has_key?(changeset.errors, :type)
+  test "new/0 returns a default struct" do
+    assert Payment.new() == %Payment{
+      signature: "",
+      amount: "",
+      currency: "RON",
+      timezone: "Europe/Bucharest",
+      token: "",
+      email: "",
+      type: "person",
+      first_name: "",
+      last_name: "",
+      address: "",
+      mobile_phone: "",
+      confirm_url: "",
+      return_url: "",
+    }
   end
 end
